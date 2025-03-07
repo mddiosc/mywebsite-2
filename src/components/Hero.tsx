f4bfdd3f4d8f12f7e5106f663067e5f867e2c8fe
Miguel Ángel de Dios <mddiosc@gmail.com>
2025-03-07 20:33:37 +0100

    fix(home): layout fixes

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -7,7 +7,7 @@ export default function Hero() {
   return (
     <div className="relative bg-white">
       <div className="mx-auto max-w-7xl lg:grid lg:grid-cols-12 lg:gap-x-8 lg:px-8">
-        <div className="px-6 pt-10 pb-24 sm:pb-32 lg:col-span-7 lg:px-0 lg:pt-40 lg:pb-48 xl:col-span-6">
+        <div className="px-6 pb-24 sm:pb-32 lg:col-span-7 lg:px-0 lg:pt-40 lg:pb-48 xl:col-span-6">
           <div className="mx-auto max-w-lg lg:mx-0">
             <h1 className="mt-24 text-5xl font-semibold tracking-tight text-pretty text-gray-900 sm:mt-10 sm:text-7xl">
               {t('components.hero.title')}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,7 @@ import { Hero } from '../components'
 
 const Home = () => {
   return (
-    <div className="flex min-h-full items-center justify-center bg-white px-6 py-24 sm:py-32 lg:px-8">
+    <div className="flex min-h-full items-center justify-center bg-white px-6 lg:px-8">
       <Hero />
     </div>
   )
