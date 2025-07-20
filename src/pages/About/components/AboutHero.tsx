58ce7619978670351827e09d0034509e82e0b593
Miguel Ángel de Dios <mddiosc@gmail.com>
2025-07-20 16:40:40 +0200

    fix(deploy): deploy issue

--- src/pages/About/components/AboutHero.tsx
+++ src/pages/About/components/AboutHero.tsx
@@ -14,7 +14,7 @@ const AboutHero = ({ biographyParagraphs }: AboutHeroProps) => {
   const { t, i18n } = useTranslation()
 
   return (
-    <div className="relative isolate -z-10">
+    <div className="relative isolate">
       <svg
         aria-hidden="true"
         className="absolute inset-x-0 top-0 -z-10 h-[64rem] w-full stroke-gray-200 [mask-image:radial-gradient(32rem_32rem_at_center,white,transparent)]"
