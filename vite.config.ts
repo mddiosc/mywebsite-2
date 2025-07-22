7a9c3f109f3f550e63101c2737437690faf57b41
Miguel Ángel de Dios <mddiosc@gmail.com>
2025-07-22 18:39:30 +0200

    feat(faro): improve grafana faro config

--- vite.config.ts
+++ vite.config.ts
@@ -7,22 +7,19 @@ import { defineConfig } from 'vite'
 import { configDefaults } from 'vitest/config'
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig(() => ({
   plugins: [
     react(),
     tailwindcss(),
-    ...(mode === 'production' && process.env.VITE_GRAFANA_FARO_API_KEY
-      ? [
-          faroUploader({
-            appName: 'mywebsite2.0',
-            endpoint: process.env.VITE_GRAFANA_FARO_ENDPOINT ?? '',
-            appId: process.env.VITE_GRAFANA_FARO_APP_ID ?? '',
-            stackId: process.env.VITE_GRAFANA_FARO_STACK_ID ?? '',
-            apiKey: process.env.VITE_GRAFANA_FARO_API_KEY,
-            gzipContents: true,
-          }),
-        ]
-      : []),
+
+    faroUploader({
+      appName: 'mywebsite2.0',
+      endpoint: process.env.VITE_GRAFANA_FARO_ENDPOINT ?? '',
+      appId: process.env.VITE_GRAFANA_FARO_APP_ID ?? '',
+      stackId: process.env.VITE_GRAFANA_FARO_STACK_ID ?? '',
+      apiKey: process.env.VITE_GRAFANA_FARO_API_KEY ?? '',
+      gzipContents: true,
+    }),
   ],
   build: {
     sourcemap: true,
