a72c255721c825b1a17fb69acf2fb0c7ed537ff6
Miguel Ángel de Dios <mddiosc@gmail.com>
2025-08-16 23:39:31 +0200

    feat: add scroll reset functionality on route navigation
    - Create ScrollToTop component to reset scroll position on route changes
    - Integrate ScrollToTop component in App.tsx globally
    - Export ScrollToTop from components index
    - Fixes scroll persistence issue on mobile navigation
    
    Improves UX by ensuring users start at top of new pages when navigating.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router } from 'react-router'
 
+import { ScrollToTop } from './components'
 import { AppRoutes } from './router/routes'
 import './i18n/i18n'
 import './styles/App.css'
@@ -7,6 +8,7 @@ import './styles/App.css'
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <AppRoutes />
     </Router>
   )
